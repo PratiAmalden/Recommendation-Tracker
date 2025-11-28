3518a60dc6566cdbb8bc861f7319740cf3bc1f42
sarawone <sarawone.poudal@gmail.com>
2025-11-28 11:20:46 +0000

    fixing item name& mood option

--- frontend/src/components/ListUI.jsx
+++ frontend/src/components/ListUI.jsx
@@ -7,9 +7,9 @@ function ListCard({items}){
         <div className="card-list">
             {items.map( (item,index) => (
                 <div className="card" key={index}>
-                    <h3 className="item-title">{item.title}</h3>
+                    <h3 className="item-title">{item.item_name}</h3>
                     <p>Category: {item.category}</p>
-                    <p>Mood: {item.mood.join(", ")}</p>
+                    <p>Mood: {item.moods.map(m=>m.name).join(", ")}</p>
                     <p>Recommender: {item.recommender}</p>
                 </div>
             )
